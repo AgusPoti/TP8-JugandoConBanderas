8c2ae1326b565f33a215a164c61e40f84005f3e5
Valenkra <47866545@ort>
2024-08-01 10:42:12 -0300

    agregado de pantallas

---
+++ app/Juego/page.js
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Input from "./../Components/Input";
+import Bandera from "./../Components/Bandera";
+import styles from './juego.module.css';
+import bStyles from './../Components/Button/Button.module.css';
+
+export default function Home() {
+  const [banderaElegida,  setBanderaElegida] = useState({});
+  const [banderas,  setBanderas] = useState([]);
+  const [puntos, setPuntos] = useState();
+  const [update, setUpdate] = useState(false);
+
+  useEffect(() => {
+    var keys = Object.keys(banderas);
+    setBanderaElegida(banderas[keys[ keys.length * Math.random() << 0]]);
+    //setBanderas(Object.filter(p => {banderas, p => p.name !== banderaElegida.name;}));
+    setUpdate(false);
+  }, [update]);
+
+  useEffect(() => {
+    axios.get("https://countriesnow.space/api/v0.1/countries/flag/images")
+      .then(
+        response => {
+          setBanderas(response.data["data"]);
+          })
+    setUpdate(true);
+  }, []);
+
+
+  return (
+    <main className={styles.main}>
+      <h1>FLAGPARDY</h1>
+      <Bandera url={(banderaElegida !== undefined) ? banderaElegida.flag : ""}/>
+      <Input iPlaceholder={"¿A qué país pertenece la bandera?"}/>
+      <button className={bStyles.boton} onClick={()=> setUpdate(true)}>Enviar</button>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,42 +1,15 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import axios from 'axios';
-import Input from "./Components/Input";
-import Bandera from "./Components/Bandera";
-import Button from "./Components/Button";
+import Head from 'next/head';
 import styles from './page.module.css';
-import bStyles from './Components/Button/Button.module.css';
+import Link from "next/link";
 
 export default function Home() {
-  const [banderaElegida,  setBanderaElegida] = useState({});
-  const [banderas,  setBanderas] = useState([]);
-  const [puntos, setPuntos] = useState();
-  const [update, setUpdate] = useState(false);
-
-  useEffect(() => {
-    var keys = Object.keys(banderas);
-    setBanderaElegida(banderas[keys[ keys.length * Math.random() << 0]]);
-    //setBanderas(Object.filter(p => {banderas, p => p.name !== banderaElegida.name;}));
-    setUpdate(false);
-  }, [update]);
-
-  useEffect(() => {
-    axios.get("https://countriesnow.space/api/v0.1/countries/flag/images")
-      .then(
-        response => {
-          setBanderas(response.data["data"]);
-          })
-    setUpdate(true);
-  }, []);
-
-
   return (
     <main className={styles.main}>
       <h1>FLAGPARDY</h1>
-      <Bandera url={(banderaElegida !== undefined) ? banderaElegida.flag : ""}/>
-      <Input iPlaceholder={"¿A qué país pertenece la bandera?"}/>
-      <button className={bStyles.boton} onClick={()=> setUpdate(true)}>Enviar</button>
+      <Link href="/Juego" className={styles.link}>Jugar</Link>
+      <Link href="/Ranking" className={styles.link}>Ranking</Link>
     </main>
   );
 }
