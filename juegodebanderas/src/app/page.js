9ae8141462d4bafabffb30870d9a936e3224cf5c
Valenkra <47866545@ort>
2024-08-01 08:21:23 -0300

    banderas cargadas

--- juegodebanderas/src/app/page.js
+++ juegodebanderas/src/app/page.js
@@ -4,35 +4,20 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [pokemones,  setPokemones] = useState({});
-  const [listado,  setListado] = useState([]);
+  const [banderaElegida,  setBanderaElegida] = useState({});
+  const [banderas,  setBanderas] = useState({});
 
   useEffect(() => {
     axios.get("https://countriesnow.space/api/v0.1/countries/flag/images")
       .then(response => {
-        setPokemones(response.data);
-        setListado(l => response.data.results);
+        setBanderas(response.data["data"]);
       })
 
-
-    console.log("hola");
   }, []);
-
-  const cargarMas = () => {
-    axios.get(pokemones.next)
-      .then(response => {
-        setPokemones(response.data);
-        setListado(l => [...l, ...response.data.results]);
-      })
-  }
     
   return (
     <main>
-      <h1>Los pokemones son {pokemones.count} </h1>
-      <ul>
-        { listado.map(pokemon => <li>{pokemon.name}</li>) }
-      </ul>
-      <button onClick={cargarMas}>Cargar más</button>
+      <h1>Los pokemones son {banderas.count} </h1>
     </main>
   );
 }
