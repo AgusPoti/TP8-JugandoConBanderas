ce9c9d6f246bcc218f5f6a690a24743ff37789e7
Android Studio <devandroid@ort>
2024-08-01 09:42:02 -0300

    intentando que no se rompa!!

---
+++ juegodebanderas/src/app/Components/Bandera/index.js
@@ -0,0 +1,7 @@
+import styles from "./Bandera.module.css";
+
+export default function Bandera() {
+  return (
+    <></>
+  );
+}
--- juegodebanderas/src/app/page.js
+++ juegodebanderas/src/app/page.js
@@ -2,24 +2,29 @@
 
 import { useEffect, useState } from "react";
 import axios from 'axios';
-
+import Input from "./Components/Input";
+import Bandera from "./Components/Bandera";
+import Button from "./Components/Button";
 
 export default function Home() {
-  const [banderaElegida,  setBanderaElegida] = useState({});
-  const [banderas,  setBanderas] = useState({});
+  const [banderaElegida, setBanderaElegida] = useState({});
+  const [banderas, setBanderas] = useState({});
   const [puntos, setPuntos] = useState();
+
 
   useEffect(() => {
     axios.get("https://countriesnow.space/api/v0.1/countries/flag/images")
       .then(response => {
         setBanderas(response.data["data"]);
-      })
-
+      });
   }, []);
-    
+
   return (
     <main>
-      <h1>Los pokemones son {banderas.count} </h1>
+      <h1>El juego de las banderas!</h1>
+      <Input iType="text" iPlaceholder="Ingrese la bandera" />
+      <Boton sendText="Finalizar" onClick={handleOpenAlert} />
+
     </main>
   );
 }
